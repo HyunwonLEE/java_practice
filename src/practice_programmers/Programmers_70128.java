a5dea074dc7a6c277dadae2d62bb90d3c99ed923
HyunwonLEE <dl102030@naver.com>
2021-05-27 15:14:57 +0200

    zz

--- src/practice_programmers/Programmers_70128.java
+++ src/practice_programmers/Programmers_70128.java
@@ -2,7 +2,7 @@ package practice_programmers;
 
 class Solution_70128 {
     public int solution(int[] a, int[] b) {
-        int answer = 1234567890;
+        int answer = 0;
         
         for(int i = 0; i < a.length; i++) {
         	answer += a[i]*b[i];
