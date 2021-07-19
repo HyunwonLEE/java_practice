97d212c368a6734b10488ba0998daf628a6c398d
94100550 <94100550@LCSC15V510.corp.lgcns.com>
2021-07-19 20:56:35 +0200

    7/17

--- src/practice_programmers/Programmers_12899.java
+++ src/practice_programmers/Programmers_12899.java
@@ -1,9 +1,31 @@
 package practice_programmers;
 
+class Solution_12899 {
+	public String solution(long n) {
+		String answer = "";
+		String[] numbers = { "1", "2", "4" };
+		int a = (int) (n / 3);
+		int b = (int) (n % 3);
+		do {
+			a = (int) (a / 3);
+			if (a > 0)
+				answer += numbers[a - 1];
+			else
+				answer += numbers[(int) (n - 1)];
+		} while (a > 3);
+
+		if (n > 3)
+			answer += numbers[b - 1];
+		return answer;
+	}
+}
+
 public class Programmers_12899 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Solution_12899 s = new Solution_12899();
+		long n = 9;
+		System.out.println(s.solution(n));
 
 	}
 
