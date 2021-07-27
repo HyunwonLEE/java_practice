92cea997ae5713c15c123eabc48672a44852a62f
94100550 <94100550@LCSC15V510.corp.lgcns.com>
2021-07-27 13:48:21 +0200

    7/27

--- src/practice_programmers/Programmers_12909.java
+++ src/practice_programmers/Programmers_12909.java
@@ -1,5 +1,34 @@
 package practice_programmers;
 
+//import java.util.Stack;
+//
+//class Solution {
+//    boolean solution(String s) {
+//        boolean answer = true;
+//        int counter = 0;
+//        
+//        Stack<Character> st = new Stack<>();
+//        
+//        for(int i = 0; i<s.length(); i++) {
+//        	if(s.charAt(i) == '(') {
+//        		st.add(s.charAt(i));
+//        	} else {        		
+//        		if (st.size() > 0){
+//        			st.pop();
+//        		}else{
+//        			answer = false;
+//        			break;
+//        		}        		
+//        	}
+//        }
+//        
+//        if (st.size() != 0)
+//        	answer = false;  
+//        		
+//        return answer;
+//    }
+//}
+
 class Solution_12909 {
 	boolean solution(String s) {
 		boolean answer = true;
