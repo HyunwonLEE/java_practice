8f7147e8488437271f02a9a1693179505dbc4acd
94100550 <94100550@LCSC15V510.corp.lgcns.com>
2021-08-03 08:30:32 +0200

    8/3

---
+++ src/practice_programmers/Programmers_42587.java
@@ -0,0 +1,21 @@
+package practice_programmers;
+
+
+class Solution_42587 {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
+public class Programmers_42587 {
+
+	public static void main(String[] args) {
+		int[] priorites = {1, 1, 9, 1, 1, 1};
+		int location = 0;
+		Solution_42587 s = new Solution_42587();
+		s.solution(priorites, location);
+
+	}
+
+}
