ecaae8b8e4107bfbb39a8c9d845d0ee708d627f2
HyunwonLEE <dl102030@naver.com>
2021-08-04 23:59:24 +0200

    8/4

--- src/coding_Test_369/Recursive.java
+++ src/coding_Test_369/Recursive.java
@@ -3,7 +3,22 @@ package coding_Test_369;
 public class Recursive {
 
 	public static void main(String[] args) {
+		Factorial fObject = new Factorial();
+		System.out.println(fObject.factorialFunc(6));
 
 	}
 
+}
+
+class Factorial {
+	public int factorialFunc(int data) {
+		if (data == 1) {
+			return 1;
+		} else if (data == 2) {
+			return 2;
+		} else if (data == 3) {
+			return 4;
+		}
+		return this.factorialFunc(data - 1) + this.factorialFunc(data - 2) + this.factorialFunc(data - 3);
+	}
 }
