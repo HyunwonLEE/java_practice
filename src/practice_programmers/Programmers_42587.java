4346a4ee6c0e192ea79adbbd2033cc756471f103
94100550 <94100550@LCSC15V510.corp.lgcns.com>
2021-08-09 08:15:47 +0200

    8/9

--- src/practice_programmers/Programmers_42587.java
+++ src/practice_programmers/Programmers_42587.java
@@ -1,9 +1,27 @@
 package practice_programmers;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 class Solution_42587 {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
+        int answer = location;
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < priorities.length; i++) {
+        	queue.add(priorities[i]);
+        }
+        for(int index = 0; index < queue.size() -1; index++) {
+        	boolean swap = false;
+        	for(int index2 = 1; index2 < queue.size(); index2++) {
+        		if(queue.peek() < priorities[index2]) {
+        			queue.add(queue.peek());
+        			queue.poll();        			
+        		}
+        	}
+
+        }
+        
         
         return answer;
     }
