be7d625ba284bba22dabcc2348f46c586ee49be0
94100550 <94100550@LCSC15V510.corp.lgcns.com>
2021-08-19 10:11:50 +0200

    8/18

---
+++ src/programmers_weekly/Third_week.java
@@ -0,0 +1,16 @@
+package programmers_weekly;
+
+class Solution_84021 {
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = -1;
+        return answer;
+    }
+}
+public class Third_week {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
