63c07ed320e32a509e63a415f2dd97d59d6e0870
94100550 <94100550@LCSC15V510.corp.lgcns.com>
2021-08-20 11:01:57 +0200

    8/20

--- src/practice_programmers/Programmers_42577.java
+++ src/practice_programmers/Programmers_42577.java
@@ -1,7 +1,6 @@
 package practice_programmers;
 //해설 보고 풀었음
 
-
 class Solution_42577 {
 	public boolean solution(String[] phone_book) {
 
@@ -47,16 +46,23 @@ class Solution_42577 {
  * } } return answer; } }
  * 
  */
-/*
- * import java.util.HashMap;
- * 
- * class Solution_42577 { public boolean solution(String[] phone_book) {
- * HashMap<String, Integer> hashMap = new HashMap<>();
- * 
- * for (String number : phone_book) hashMap.put(number, 0); for (String key :
- * hashMap.keySet()) for (int j = 1; j <= key.length() - 1; j++) if
- * (hashMap.containsKey(key.substring(0, j))) return false; return true; } }
- */
+
+//import java.util.HashMap;
+//
+//class Solution_42577 {
+//	public boolean solution(String[] phone_book) {
+//		HashMap<String, Integer> hashMap = new HashMap<>();
+//
+//		for (String number : phone_book)
+//			hashMap.put(number, 0);
+//		for (String key : hashMap.keySet())
+//			for (int j = 1; j <= key.length() - 1; j++)
+//				if (hashMap.containsKey(key.substring(0, j)))
+//					return false;
+//		return true;
+//	}
+//}
+
 public class Programmers_42577 {
 
 	public static void main(String[] args) {
