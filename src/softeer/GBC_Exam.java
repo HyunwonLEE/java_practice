74c710f44528a6247672a3ba04a3eee204ecd695
HyunwonLEE <dl102030@naver.com>
2021-10-18 21:29:11 +0200

    10/18

--- src/softeer/GBC_Exam.java
+++ src/softeer/GBC_Exam.java
@@ -8,49 +8,60 @@ public class GBC_Exam {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int N = Integer.parseInt(sc.next());
 		int M = Integer.parseInt(sc.next());
-		
+
 		ArrayList<int[]> list = new ArrayList<int[]>();
-		
-		for(int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			int[] arr = new int[2];
 			arr[0] = sc.nextInt();
 			arr[1] = sc.nextInt();
 			list.add(arr);
 		}
 		ArrayList<int[]> list1 = new ArrayList<int[]>();
-		
-		for(int i = 0; i < M; i++) {
+
+		for (int i = 0; i < M; i++) {
 			int[] arr1 = new int[2];
 			arr1[0] = sc.nextInt();
 			arr1[1] = sc.nextInt();
 			list1.add(arr1);
 		}
-		int index = 0;
 		int max = 0;
+		int first = 0;
+		int second = 0;
+		
 		while (list.size() > 0 && list1.size() > 0) {
-			
-			list.get(index)[0] = list.get(index)[0] - list1.get(index)[0];
-			list1.get(index)[0] = list1.get(index)[0] - list.get(index)[0];
-			if(list.get(index)[1] - list1.get(index)[1] > max) {
-				max = list.get(index)[1] - list1.get(index)[1];
-			}
-			if(list.get(index)[0] < 0) {
-				list.remove(index);
+			int temp = 0;
+			first = list.get(0)[0];
+			second = list1.get(0)[0];
+
+			if (list.get(0)[1] < list1.get(0)[1]) {
+				temp = list1.get(0)[1] - list.get(0)[1];
+				if(max < temp) {
+					max = temp;
+				}
 				
 			}
-			if(list1.get(index)[0] < 0) {
-				list1.remove(index);
-				
+			if (first < second) {
+				list1.get(0)[0] = second - first;
+				first = 0;
+				list.remove(0);
+			} else if (first > second) {
+				list.get(0)[0] = first - second;
+				second = 0;
+				list1.remove(0);
+			} else {				
+				list.remove(0);
+				list1.remove(0);
+				first = 0;
+				second = 0;
 			}
-				
+
 		}
 		System.out.println(max);
-		
-		
-		
+
 	}
 
 }
