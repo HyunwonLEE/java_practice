fdf22191f4a70451f71ab961f56fd8350dbf045a
HyunwonLEE <dl102030@naver.com>
2021-10-19 22:24:11 +0200

    10/19

--- src/softeer/장애물인식프로그램_Exam.java
+++ src/softeer/장애물인식프로그램_Exam.java
@@ -8,9 +8,17 @@ public class 장애물인식프로그램_Exam {
 
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		int[][] arr = new int[N][N];
 		for(int i = 0; i < N; i++) {
 			String str = sc.next();
+			
+			for(int j = 0; j < str.length(); j++) {
+				arr[i][j] = str.charAt(j) - 48;
+			}
+			
 		}
+		
+		System.out.println("end");
 	}
 
 }
